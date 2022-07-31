da48596ab1a0236c0ba5cd27210122579af37c3a
Mariano San Juan <marianosanjuan90@gmail.com>
2022-07-31 08:37:03 -0300

    ajuste de traduccion

--- appblog/models.py
+++ appblog/models.py
@@ -12,6 +12,6 @@ class Posteos(models.Model):
     fecha_creacion = models.DateField(null=True)
     
     def __str__(self):
-        return (f'Este posteo pertenece a {self.autor}')
+        return (f'This post belongs to {self.autor}')
     
     
