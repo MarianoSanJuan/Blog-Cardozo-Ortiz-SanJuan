f515bade57fc2d2f5aaef7b745b5820102663b3c
Mariano San Juan <marianosanjuan90@gmail.com>
2022-07-31 18:54:24 -0300

    Correcion error

--- appblog/models.py
+++ appblog/models.py
@@ -13,6 +13,6 @@ class Posteos(models.Model):
     fecha_creacion = models.DateField(null=True)
     
     def __str__(self):
-        return (f'Este posteo pertenece a {self.autor}')
+        return (f'This posts belongs to {self.autor}')
     
     
