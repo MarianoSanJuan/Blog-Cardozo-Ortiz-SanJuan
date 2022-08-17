6939a54637db018ac2bcadd6491970988b51125b
Mariano San Juan <marianosanjuan90@gmail.com>
2022-08-17 18:16:20 -0300

    Arreglo de template para ver perfiles desde urls

--- appaccounts/models.py
+++ appaccounts/models.py
@@ -7,3 +7,5 @@ from django.contrib.auth.models import User
 class MasDatosUsuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatares', null=True, blank=True)
+    # Agregado de campo adicional al user
+    # description = models.CharField(max_length=50)
