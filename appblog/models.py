a9b2fc1f6ed5d600a56b101dd91afc0bf9d4e6cd
Mariano San Juan <marianosanjuan90@gmail.com>
2022-08-18 08:07:50 -0300

    Crear posteo y editar posteo ahora aceptan la imagen

--- appblog/models.py
+++ appblog/models.py
@@ -11,7 +11,7 @@ class Posteos(models.Model):
     contenido = models.CharField(max_length=300)
     autor = models.CharField(max_length=30)
     fecha_creacion = models.DateField(null=True)
-    #test
+    #prueba image
     image = models.ImageField(upload_to='imagepost', null=True, blank=True)
     
     def __str__(self):
